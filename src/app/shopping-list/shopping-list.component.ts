b99c24bb1fe01a5c392cde0f48bcd7c0c9a25f82
sanketpatel <patelsanketr98@gmail.com>
2023-01-06 16:07:24 -0500

    Shopping list add feature working

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -11,4 +11,8 @@ export class ShoppingListComponent {
     new Ingredient('Apples', 5),
     new Ingredient('Banana', 9)
   ];
+
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient)
+  }
 }
